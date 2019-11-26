05382a42f9594827b246e858d27fec746b92cf19
1059431281 <1059431281@qq.con>
2019-11-26 15:34:42 +0800

    work13

--- work12-1/index1.js
+++ work12-1/index1.js
@@ -3,13 +3,10 @@ let year = new Date().getFullYear()
 document.getElementById("cal").innerHTML = calendar(year)
 
 function setCalendar(year) {
-    document.getElementById('cal').innerHTML = '<div class="box" id="cal"></div>'
-
+    document.getElementById('cal').innerHTML = ''
     var year = parseInt(year)
     var pre = year-1
     var next = year+1
-    console.log(pre)
-    console.log(next)
     document.getElementById('curyear').setAttribute('data-year', year);
     document.getElementById('preyear').setAttribute('data-year', pre);
     document.getElementById('nextyear').setAttribute('data-year', next);
