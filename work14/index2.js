c591bceb02d117d4c154a02ee4c055724ff7e5d0
1059431281 <1059431281@qq.con>
2019-12-03 16:08:18 +0800

    work15

--- work14/index2.js
+++ work14/index2.js
@@ -2,18 +2,18 @@ var date = new Date()
 let h1=m1=s1=0
 
 m1 = 59-date.getMinutes()
-s1 = 60-date.getSeconds()
+s1 = 59-date.getSeconds()
 
-let id1 =setInterval(seckill,1000)
+let id1 =setInterval(seckill,1)
 
 function seckill() {
     s1--
-    if(s1==0){
-        s1=60
+    if(s1==-1){
+        s1=59
         m1--
     }
-    if(m1==0){
-        m1=60
+    if(m1==-1){
+        m1=59
     }
     document.getElementById('m1').innerHTML = m1 +'分'
     document.getElementById('s1').innerHTML = s1 +'秒'
