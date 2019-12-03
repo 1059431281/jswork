5e60610e93f65c892602a56882fe7c398cb22436
1059431281 <1059431281@qq.con>
2019-12-03 15:40:53 +0800

    work15

--- work14/index2.js
+++ work14/index2.js
@@ -4,22 +4,17 @@ let h1=m1=s1=0
 h1 = date.getHours()
 m1 = date.getMinutes()
 s1 = date.getSeconds()
-let id1 =setInterval(seckill,1000)
+let id1 =setInterval(seckill,1)
 
 function seckill() {
-    s1++
-    if(s1==60){
-        m1++
-        s1 = '00'
+    s1--
+    if(s1==0){
+        s1=60
+        m1--
     }
-    if(m1==60){
-        h1++
-        m1='00'
+    if(m1==0){
+        m1=60
     }
-    if(h1==24){
-        h1='00'
-    }
-    document.getElementById('h1').innerHTML = h1 +'时'
     document.getElementById('m1').innerHTML = m1 +'分'
     document.getElementById('s1').innerHTML = s1 +'秒'
 }
